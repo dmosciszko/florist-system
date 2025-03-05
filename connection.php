d2ef6fc7012b41e6351d90580101a244c7765d78
dmosciszko <77761374+dmosciszko@users.noreply.github.com>
2025-03-05 15:40:18 -0500

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php 
-  $con = mysqli_connect ("sql1.njit.edu", "dm634", "Warwick2018!", "dm634");
+  $con = mysqli_connect ("", "", "", "");
   if (mysqli_connect_errno())  {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
